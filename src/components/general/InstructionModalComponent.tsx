a85c9d707500673458763f7e22f04abc4bf737a6
rpulkka <robert.pulkka@gmail.com>
2021-05-07 17:00:06 +0300

    Added new translations and fixed small UI bugs. Renamed atlasCode -field in forms.

--- src/components/general/InstructionModalComponent.tsx
+++ src/components/general/InstructionModalComponent.tsx
@@ -3,7 +3,8 @@ import { Linking, View, FlatList, Text } from 'react-native'
 import Modal from 'react-native-modal'
 import { useTranslation } from 'react-i18next'
 import Cs from '../../styles/ContainerStyles'
-import { vihkoEn, vihkoFi, vihkoSv } from '../../config/urls'
+import { vihkoEn, vihkoFi, vihkoSv, instructionsEn, instructionsFi, instructionsSv } from '../../config/urls'
+import { ScrollView } from 'react-native-gesture-handler'
 
 type Props = {
   isVisible: boolean,
@@ -14,37 +15,53 @@ const InstructionModalComponent = (props: Props) => {
   const { t, i18n } = useTranslation()
 
   let vihko = ''
+  let instructions = ''
 
   if (i18n.language === 'fi') {
     vihko = vihkoFi
+    instructions = instructionsFi
   } else if (i18n.language === 'sv') {
     vihko = vihkoSv
+    instructions = instructionsSv
   } else {
     vihko = vihkoEn
+    instructions = instructionsEn
   }
 
   return (
     <Modal isVisible={props.isVisible} onBackButtonPress={props.onClose} onBackdropPress={props.onClose}>
       <View style={Cs.observationAddModal}>
-        <FlatList
-          data={[...Array(7).keys()].map(val => { return { key: `${val + 1}` } })}
-          renderItem={({ item }) =>
-            <View style={Cs.instructionContainer}>
-              <Text>{t('instructions.mobilevihko.' + item.key)}</Text>
-            </View>
-          }
-        />
-        <Text style={{ paddingLeft: 10 }}>
-          <Text>
-            {t('instructions.mobilevihko.8')}
-          </Text>
-          <Text style={{ color: 'blue' }} onPress={() => Linking.openURL(vihko)}>
-            {' ' + `${t('instructions.mobilevihko.link')}`}
-          </Text>
-          <Text>
-            {t('instructions.mobilevihko.9')}
-          </Text>
-        </Text>
+        <ScrollView>
+          <View>
+            <FlatList
+              data={[...Array(7).keys()].map(val => { return { key: `${val + 1}` } })}
+              renderItem={({ item }) =>
+                <View style={Cs.instructionContainer}>
+                  {console.log(t('instructions.mobilevihko.' + item.key))}
+                  <Text>{t('instructions.mobilevihko.' + item.key)}</Text>
+                </View>
+              }
+            />
+            <Text>
+              <Text>
+                {t('instructions.mobilevihko.8')}
+              </Text>
+              <Text style={{ color: 'blue' }} onPress={() => Linking.openURL(vihko)}>
+                {' ' + `${t('instructions.mobilevihko.link')}`}
+              </Text>
+              <Text>
+                {t('instructions.mobilevihko.9')}
+                {'\n\n'}
+              </Text>
+              <Text>
+                {t('instructions.mobilevihko.10')}
+              </Text>
+              <Text style={{ color: 'blue' }} onPress={() => Linking.openURL(instructions)}>
+                {' ' + `${'https://laji.fi/about/4981'}`}
+              </Text>
+            </Text>
+          </View>
+        </ScrollView>
       </View>
     </Modal>
   )
