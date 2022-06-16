3a67d6acc9d0af5adad13280480c4286ea8b4f00
rpulkka <robert.pulkka@gmail.com>
2022-06-16 11:28:15 +0300

    Adding first tests: Test module for version number helper. Jest configurations.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:jest/recommended'
   ],
   'globals': {
     'Atomics': 'readonly',
--- src/helpers/versionHelper.tsx
+++ src/helpers/versionHelper.tsx
@@ -1,15 +1,9 @@
-import AppJSON from '../../app.json'
-import { getVersionNumber } from "../services/versionService"
+export const updateIsAvailable = async (appVersion: string, latestVersion: string): Promise<boolean> => {
+  const appVersionArray = appVersion.split('.')
+  const latestVersionArray = latestVersion.split('.')
 
-export const updateIsAvailable = async (): Promise<boolean> => {
-    const appVersion = AppJSON.expo.version
-    const latestVersion = await getVersionNumber()
-
-    const appVersionArray = appVersion.split('.')
-    const latestVersionArray = latestVersion.split('.')
-
-    if (appVersionArray[0] < latestVersionArray[0]) return true
-    else if (appVersionArray[1] < latestVersionArray[1]) return true
-    else if (appVersionArray[2] < latestVersionArray[2]) return true
-    else return false
+  if (appVersionArray[0] < latestVersionArray[0]) return true
+  else if (appVersionArray[1] < latestVersionArray[1]) return true
+  else if (appVersionArray[2] < latestVersionArray[2]) return true
+  else return false
 }
