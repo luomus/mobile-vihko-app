94fb19bc9ab1005e52b3c5caa32b15a55408cc32
rpulkka <robert.pulkka@gmail.com>
2022-08-23 14:00:49 +0300

    Fixed the notification about version updates by disabling cache from the Axios GET.

--- src/services/versionService.tsx
+++ src/services/versionService.tsx
@@ -2,6 +2,6 @@ import axios from 'axios'
 import { versionNumberUrl } from '../config/urls'
 
 export const getVersionNumber = async () => {
-  const result = await axios.get(versionNumberUrl)
+  const result = await axios.get(versionNumberUrl, { headers: { 'Cache-Control': 'no-cache' }})
   return result.data
 }
