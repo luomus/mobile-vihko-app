eecf9159a440306bd5fa401109d7a8cbe0985388
Christoffer Ackelman <christoffer.ackelman@perfektio.fi>
2022-12-23 12:50:56 +0200

    Fix so autocomplete mock returns the same type of object {query: ..., result: []} as the real autocomplete

--- src/services/__mocks__/autocompleteService.tsx
+++ src/services/__mocks__/autocompleteService.tsx
@@ -1,116 +1,119 @@
 import { Canceler } from 'axios'
 
 export const getTaxonAutocomplete = async (target: string, q: string, filters: Record<string, any> | null, lang: string, limit: number, setCancelToken: ((c: Canceler) => void) | null) => {
-  return [
-    {
-      'key': 'MX.73566',
-      'value': 'varis',
-      'payload': {
-        'matchingName': 'varis',
-        'informalTaxonGroups': [
-          {
-            'id': 'MVL.1',
-            'name': 'Linnut'
-          }
-        ],
-        'scientificName': 'Corvus corone',
-        'scientificNameAuthorship': 'Linnaeus, 1758',
-        'taxonRankId': 'MX.species',
-        'matchType': 'partialMatches',
-        'cursiveName': true,
-        'finnish': true,
-        'species': true,
-        'nameType': 'MX.vernacularName',
-        'vernacularName': 'varis'
+  return {
+    query: q,
+    result: [
+      {
+        'key': 'MX.73566',
+        'value': 'varis',
+        'payload': {
+          'matchingName': 'varis',
+          'informalTaxonGroups': [
+            {
+              'id': 'MVL.1',
+              'name': 'Linnut'
+            }
+          ],
+          'scientificName': 'Corvus corone',
+          'scientificNameAuthorship': 'Linnaeus, 1758',
+          'taxonRankId': 'MX.species',
+          'matchType': 'partialMatches',
+          'cursiveName': true,
+          'finnish': true,
+          'species': true,
+          'nameType': 'MX.vernacularName',
+          'vernacularName': 'varis'
+        }
+      },
+      {
+        'key': 'MX.36287',
+        'value': 'vihervarpunen',
+        'payload': {
+          'matchingName': 'vihervarpunen',
+          'informalTaxonGroups': [
+            {
+              'id': 'MVL.1',
+              'name': 'Linnut'
+            }
+          ],
+          'scientificName': 'Carduelis spinus',
+          'scientificNameAuthorship': '(Linnaeus, 1758)',
+          'taxonRankId': 'MX.species',
+          'matchType': 'partialMatches',
+          'cursiveName': true,
+          'finnish': true,
+          'species': true,
+          'nameType': 'MX.vernacularName',
+          'vernacularName': 'vihervarpunen'
+        }
+      },
+      {
+        'key': 'MX.34021',
+        'value': 'svartvit flugsnappare',
+        'payload': {
+          'matchingName': 'svartvit flugsnappare',
+          'informalTaxonGroups': [
+            {
+              'id': 'MVL.1',
+              'name': 'Linnut'
+            }
+          ],
+          'scientificName': 'Ficedula hypoleuca',
+          'scientificNameAuthorship': '(Pallas, 1764)',
+          'taxonRankId': 'MX.species',
+          'matchType': 'partialMatches',
+          'cursiveName': true,
+          'finnish': true,
+          'species': true,
+          'nameType': 'MX.vernacularName',
+          'vernacularName': 'kirjosieppo'
+        }
+      },
+      {
+        'key': 'MX.36573',
+        'value': 'varpunen',
+        'payload': {
+          'matchingName': 'varpunen',
+          'informalTaxonGroups': [
+            {
+              'id': 'MVL.1',
+              'name': 'Linnut'
+            }
+          ],
+          'scientificName': 'Passer domesticus',
+          'scientificNameAuthorship': '(Linnaeus, 1758)',
+          'taxonRankId': 'MX.species',
+          'matchType': 'partialMatches',
+          'cursiveName': true,
+          'finnish': true,
+          'species': true,
+          'nameType': 'MX.vernacularName',
+          'vernacularName': 'varpunen'
+        }
+      },
+      {
+        'key': 'MX.34549',
+        'value': 'svartmes',
+        'payload': {
+          'matchingName': 'svartmes',
+          'informalTaxonGroups': [
+            {
+              'id': 'MVL.1',
+              'name': 'Linnut'
+            }
+          ],
+          'scientificName': 'Periparus ater',
+          'scientificNameAuthorship': '(Linnaeus, 1758)',
+          'taxonRankId': 'MX.species',
+          'matchType': 'partialMatches',
+          'cursiveName': true,
+          'finnish': true,
+          'species': true,
+          'nameType': 'MX.vernacularName',
+          'vernacularName': 'kuusitiainen'
+        }
       }
-    },
-    {
-      'key': 'MX.36287',
-      'value': 'vihervarpunen',
-      'payload': {
-        'matchingName': 'vihervarpunen',
-        'informalTaxonGroups': [
-          {
-            'id': 'MVL.1',
-            'name': 'Linnut'
-          }
-        ],
-        'scientificName': 'Carduelis spinus',
-        'scientificNameAuthorship': '(Linnaeus, 1758)',
-        'taxonRankId': 'MX.species',
-        'matchType': 'partialMatches',
-        'cursiveName': true,
-        'finnish': true,
-        'species': true,
-        'nameType': 'MX.vernacularName',
-        'vernacularName': 'vihervarpunen'
-      }
-    },
-    {
-      'key': 'MX.34021',
-      'value': 'svartvit flugsnappare',
-      'payload': {
-        'matchingName': 'svartvit flugsnappare',
-        'informalTaxonGroups': [
-          {
-            'id': 'MVL.1',
-            'name': 'Linnut'
-          }
-        ],
-        'scientificName': 'Ficedula hypoleuca',
-        'scientificNameAuthorship': '(Pallas, 1764)',
-        'taxonRankId': 'MX.species',
-        'matchType': 'partialMatches',
-        'cursiveName': true,
-        'finnish': true,
-        'species': true,
-        'nameType': 'MX.vernacularName',
-        'vernacularName': 'kirjosieppo'
-      }
-    },
-    {
-      'key': 'MX.36573',
-      'value': 'varpunen',
-      'payload': {
-        'matchingName': 'varpunen',
-        'informalTaxonGroups': [
-          {
-            'id': 'MVL.1',
-            'name': 'Linnut'
-          }
-        ],
-        'scientificName': 'Passer domesticus',
-        'scientificNameAuthorship': '(Linnaeus, 1758)',
-        'taxonRankId': 'MX.species',
-        'matchType': 'partialMatches',
-        'cursiveName': true,
-        'finnish': true,
-        'species': true,
-        'nameType': 'MX.vernacularName',
-        'vernacularName': 'varpunen'
-      }
-    },
-    {
-      'key': 'MX.34549',
-      'value': 'svartmes',
-      'payload': {
-        'matchingName': 'svartmes',
-        'informalTaxonGroups': [
-          {
-            'id': 'MVL.1',
-            'name': 'Linnut'
-          }
-        ],
-        'scientificName': 'Periparus ater',
-        'scientificNameAuthorship': '(Linnaeus, 1758)',
-        'taxonRankId': 'MX.species',
-        'matchType': 'partialMatches',
-        'cursiveName': true,
-        'finnish': true,
-        'species': true,
-        'nameType': 'MX.vernacularName',
-        'vernacularName': 'kuusitiainen'
-      }
-    }
-  ]
+    ]
+  }
 }
