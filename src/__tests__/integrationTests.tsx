b428d768157219f9721c1bcc9a3d9c6bc1313706
Christoffer Ackelman <christoffer.ackelman@perfektio.fi>
2023-01-04 15:03:49 +0100

    Fixed errors from integrationTest

--- src/__tests__/integrationTests.tsx
+++ src/__tests__/integrationTests.tsx
@@ -14,85 +14,63 @@ let testPressLocation = {
 
 describe('Application', () => {
   it('creates and posts an event successfully', async () => {
-    const { getByText, getByTestId, getAllByTestId } = renderWithProviders(<Navigator initialRoute='login'/>)
+    const { getByText, getAllByText, getByTestId, getAllByTestId } = renderWithProviders(<Navigator initialRoute='login'/>)
 
     //LoginComponent
-    await waitFor(() => {
-      expect(getByText('Käyttääksesi sovellusta, sinun tulee kirjautua sisään.')).toBeDefined()
-      fireEvent.press(getByText('Kirjaudu sisään'))
-    })
+    await waitFor(() => expect(getByText('Käyttääksesi sovellusta, sinun tulee kirjautua sisään.')).toBeDefined())
+    fireEvent.press(getByText('Kirjaudu sisään'))
 
     //HomeComponent
-    await waitFor(() => {
-      expect(getByText('Retkilomake')).toBeDefined()
-      fireEvent.press(getByText('Retkilomake'))
-    })
+    await waitFor(() => expect(getAllByText('Retkilomake')).toHaveLength(2))
+    fireEvent.press(getByText('Retkilomake'))
 
     //FormLauncherComponent
-    await waitFor(() => {
-      expect(getByText('Aloita')).toBeDefined()
-      fireEvent.press(getByText('Aloita'))
-    })
+    await waitFor(() => expect(getByText('Aloita')).toBeDefined())
+    fireEvent.press(getByText('Aloita'))
 
     //MapComponent
-    await waitFor(() => {
-      expect(getByTestId('map-view')).toBeDefined()
-      fireEvent(getByTestId('map-view'), 'onLongPress', testPressLocation)
-    })
+    await waitFor(() => expect(getByTestId('map-view')).toBeDefined())
+    fireEvent(getByTestId('map-view'), 'onLongPress', testPressLocation)
 
     //ObservationButtonsComponent
-    await waitFor(() => {
-      expect(getByText('+ Havainto')).toBeDefined()
-      fireEvent.press(getByText('+ Havainto'))
-    })
+    await waitFor(() => expect(getByText('+ Havainto')).toBeDefined())
+    fireEvent.press(getByText('+ Havainto'))
 
     //ObservationComponent
-    await waitFor(() => {
-      expect(getByTestId('autocomplete')).toBeDefined()
-      fireEvent.changeText(getByTestId('autocomplete'), 'varis')
-      expect(getByTestId('Määrä')).toBeDefined()
-      fireEvent.changeText(getByTestId('Määrä'), '1')
-      expect(getByTestId('saveButton')).toBeDefined()
-      fireEvent.press(getByTestId('saveButton'))
-    })
+    await waitFor(() => expect(getByTestId('autocomplete')).toBeDefined())
+    fireEvent.changeText(getByTestId('autocomplete'), 'varis')
+    await waitFor(() => expect(getByTestId('Määrä')).toBeDefined())
+    fireEvent.changeText(getByTestId('Määrä'), '1')
+    await waitFor(() => expect(getByTestId('saveButton')).toBeDefined())
+    fireEvent.press(getByTestId('saveButton'))
 
     //MapComponent
-    await waitFor(() => {
-      expect(getByTestId('map-view')).toBeDefined()
-      expect(getByTestId('homeButton')).toBeDefined()
-      fireEvent.press(getByTestId('homeButton'))
-    })
+    await waitFor(() => expect(getByTestId('map-view')).toBeDefined())
+    expect(getByTestId('homeButton')).toBeDefined()
+    fireEvent.press(getByTestId('homeButton'))
 
     //HomeComponent
-    await waitFor(() => {
-      expect(getByText('Käynnissä oleva havaintotapahtuma')).toBeDefined()
-      expect(getByText('Havaintoja: 1 kpl')).toBeDefined()
-      expect(getByTestId('stopFromHomeComponent')).toBeDefined()
-      fireEvent.press(getByTestId('stopFromHomeComponent'))
-      //expect(within(getByTestId('stopFromHomeMessage')).getByText('Lopeta'))
-      const topMessage = getAllByTestId('stopFromHomeMessage')[0]
-      fireEvent.press(within(topMessage).getByText('Lopeta'))
-    })
+    await waitFor(() => expect(getByText('Käynnissä oleva havaintotapahtuma')).toBeDefined())
+    expect(getByText('Havaintoja: 1 kpl')).toBeDefined()
+    expect(getByTestId('stopFromHomeComponent')).toBeDefined()
+    fireEvent.press(getByTestId('stopFromHomeComponent'))
+    //expect(within(getByTestId('stopFromHomeMessage')).getByText('Lopeta'))
+    const topMessage = getAllByTestId('stopFromHomeMessage')[0]
+    fireEvent.press(within(topMessage).getByText('Lopeta'))
 
     //DocumentComponent
-    await waitFor(() => {
-      expect(getByTestId('Paikannimet (kunta tallentuu automaattisesti)')).toBeDefined()
-      fireEvent.changeText(getByTestId('Paikannimet (kunta tallentuu automaattisesti)'), 'Etu-Töölö')
-      expect(getByTestId('Sää')).toBeDefined()
-      fireEvent.changeText(getByTestId('Sää'), 'Pouta')
-      expect(getByTestId('saveButton')).toBeDefined()
-      fireEvent.press(getByTestId('saveButton'))
-    })
+    await waitFor(() => expect(getByTestId('Paikannimet (kunta tallentuu automaattisesti)')).toBeDefined())
+    fireEvent.changeText(getByTestId('Paikannimet (kunta tallentuu automaattisesti)'), 'Etu-Töölö')
+    expect(getByTestId('Sää')).toBeDefined()
+    fireEvent.changeText(getByTestId('Sää'), 'Pouta')
+    expect(getByTestId('saveButton')).toBeDefined()
+    fireEvent.press(getByTestId('saveButton'))
 
     //SendEventModalComponent
-    await waitFor(() => {
-      expect(getByText('Lähetä julkisena')).toBeDefined()
-      fireEvent.press(getByText('Lähetä julkisena'))
-    })
+    await waitFor(() => expect(getByText('Lähetä julkisena')).toBeDefined())
+    fireEvent.press(getByText('Lähetä julkisena'))
 
     //HomeComponent
-    await waitFor(() => {
-      expect(getByText('Retkilomake')).toBeDefined()
-    })
+    await waitFor(() => expect(getByText('Retkilomake')).toBeDefined())
   })
 })
