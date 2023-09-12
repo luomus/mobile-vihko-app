7c420f394693ca66b359e6d74456974e8bd100e4
rpulkka <robert.pulkka@gmail.com>
2023-09-12 15:24:08 +0300

    Fixed broken button logic on Atlas instruction for iOS.

--- src/components/home/AtlasInstructionModalComponent.tsx
+++ src/components/home/AtlasInstructionModalComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 import Modal from 'react-native-modal'
 import { useTranslation } from 'react-i18next'
@@ -18,14 +18,16 @@ const AtlasInstructionModalComponent = (props: Props) => {
 
   const { t } = useTranslation()
 
-  const openGridModal = () => {
+  const [cancel, setCancel] = useState<boolean>(true)
+
+  const buttonHandler = (cancelValue: boolean) => {
+    setCancel(cancelValue)
     props.setModalVisibility(false)
-    props.setGridModalVisibility(true)
   }
 
   return (
     <Modal isVisible={props.modalVisibility} backdropOpacity={10} onBackButtonPress={() => { props.setModalVisibility(false) }}
-      onBackdropPress={() => { props.setModalVisibility(false) }}>
+      onBackdropPress={() => { props.setModalVisibility(false) }} onModalHide={() => cancel ? null : props.setGridModalVisibility(true)}>
       <View style={ Cs.atlasInstructionContainer }>
         <ScrollView>
           <Text style={Ts.homeScreenTitle}>
@@ -35,12 +37,12 @@ const AtlasInstructionModalComponent = (props: Props) => {
             {t('grid description intro') + '\n\n' + t('grid description')}
           </Text>
           <View style={Cs.modalStartButtonContainer}>
-            <ButtonComponent onPressFunction={() => openGridModal()} title={t('continue')}
+            <ButtonComponent onPressFunction={() => buttonHandler(false)} title={t('continue')}
               height={40} width={120} buttonStyle={Bs.beginButton}
               gradientColorStart={Colors.primaryButton1} gradientColorEnd={Colors.primaryButton2} shadowColor={Colors.primaryShadow}
               textStyle={Ts.buttonText} iconName={'done'} iconType={'material-icons'} iconSize={22} contentColor={Colors.whiteText}
             />
-            <ButtonComponent onPressFunction={() => props.setModalVisibility(false)} title={t('cancel')}
+            <ButtonComponent onPressFunction={() => buttonHandler(true)} title={t('cancel')}
               height={40} width={120} buttonStyle={Bs.beginButton}
               gradientColorStart={Colors.neutralButton} gradientColorEnd={Colors.neutralButton} shadowColor={Colors.neutralShadow}
               textStyle={Ts.buttonText} iconName={'cancel'} iconType={'material-icons'} iconSize={22} contentColor={Colors.darkText}
