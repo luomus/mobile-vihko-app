38b317995222ed09735baacccceaa04d2ef40915
rpulkka <robert.pulkka@gmail.com>
2024-02-22 14:26:26 +0200

    Expo 50 update.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', ['@babel/preset-env', {targets: {node: 'current'}}], '@babel/preset-typescript'],
+    presets: [['@babel/preset-env', {targets: {node: 'current'}}], 'babel-preset-expo', '@babel/preset-typescript'],
     plugins: [
       ["module:react-native-dotenv", {
         "moduleName": "react-native-dotenv"
