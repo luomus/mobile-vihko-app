e65dd54eecfbf081460e1b23be0a60a5141674c4
rpulkka <robert.pulkka@gmail.com>
2025-01-20 09:12:33 +0200

    Rollback to Expo 51.

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,5 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro')
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
 
 const config = getSentryExpoConfig(__dirname)
 
-module.exports = wrapWithReanimatedMetroConfig(config)
+module.exports = config
