e3bef992df021230370bac3978e7d5bfae0342b5
rpulkka <robert.pulkka@gmail.com>
2026-08-31 14:56:12 +0300

    Refactored userService contents to user actions.

--- src/stores/user/actions.tsx
+++ src/stores/user/actions.tsx
@@ -1,6 +1,5 @@
 import { clearCredentials, RootState, setCredentials } from '../'
 import lajiApiService from '../../api/services/lajiApiService'
-import { pollUserLogin } from '../../services/userService'
 import storageService from '../../services/storageService'
 import i18n from '../../languages/i18n'
 import { stopLocationAsync } from '../../helpers/geolocationHelper'
@@ -98,6 +97,62 @@ export const loginUser = createAsyncThunk<void, loginUserParams, { rejectValue: 
     }
   }
 )
+
+export const pollUserLogin = async (tmpToken: string, setCanceler: any) => {
+  let poller: ReturnType<typeof setInterval>
+  let timeout: ReturnType<typeof setTimeout>
+
+  const userPromise = new Promise<CredentialsType> ((resolve, reject) => {
+    const poll = async () => {
+      let result
+      try {
+        result = await lajiApiService.postLoginCheck(tmpToken)
+      } catch (error) {
+        //did not get the token in this poll
+        result = { token: undefined }
+      }
+      if (result.token) {
+        try {
+          await lajiApiService.getAuthenticationEvent(result.token)
+          const userData = await lajiApiService.getPerson(result.token)
+          clearInterval(poller)
+          clearTimeout(timeout)
+          resolve({
+            user: userData,
+            token: result.token,
+          })
+        } catch (error) {
+          captureException(error)
+          clearInterval(poller)
+          clearTimeout(timeout)
+          reject(error)
+        }
+      }
+    }
+
+    //starts a 180 second timeout which stops above polling interval
+    timeout = setTimeout(() => {
+      clearInterval(poller)
+      reject({
+        timeout: true
+      })
+    }, 180000)
+
+    //poll immediately, then every 3 seconds
+    poll()
+    poller = setInterval(poll, 3000)
+
+    setCanceler(() => () => {
+      clearInterval(poller)
+      clearTimeout(timeout)
+      reject({
+        canceled: true
+      })
+    })
+  })
+
+  return userPromise
+}
 
 export const initLocalCredentials = createAsyncThunk<void, undefined, { rejectValue: Record<string, any> }>(
   'credentials/initLocalCredentials',
